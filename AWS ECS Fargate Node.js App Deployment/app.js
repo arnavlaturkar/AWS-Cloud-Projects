b6c8b872c9b666340fa091854592bc8cb48a1c55
arnavlaturkar <laturkararnav7@gmail.com>
2025-10-04 14:36:28 +0530

    Full project upload

---
+++ AWS ECS Fargate Node.js App Deployment/app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from Arnav (Harvey) Laturkar!');
+});
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
